5baea33391072786a241fbca22eea192ef0a4cf4
YuriNem <PsychoactivePie@gmail.com>
2019-03-31 23:09:34 +0300

    Added prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,61 @@
-import React from 'react';
-import { uniqueId } from 'lodash';
+import React from "react";
+import { uniqueId } from "lodash";
 
-import './style.scss';
-const logo = require('../../svg/logo.svg');
+import "./style.scss";
+const logo = require("../../svg/logo.svg");
 
-import SidebarContainer from '../containers/Sidebar.js';
-import TicketContainer from '../containers/Ticket.js';
+import SidebarContainer from "../containers/Sidebar.js";
+import TicketContainer from "../containers/Ticket.js";
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const { asyncGetTickets, asyncOffMobileSidebar } = this.props;
+    window.addEventListener("resize", event =>
+      asyncOffMobileSidebar({ width: event.target.screen.width })
+    );
+    asyncGetTickets();
+  }
 
-    componentDidMount() {
-        const { asyncGetTickets, asyncOffMobileSidebar } = this.props;
-        window.addEventListener('resize', 
-            event => asyncOffMobileSidebar({ width: event.target.screen.width }));
-        asyncGetTickets();
-    }
+  render() {
+    const {
+      tickets,
+      currency,
+      stops: { allStops, noStops, oneStop, twoStops, threeStops },
+      mobileSidebar,
+      asyncChangeSidebarView
+    } = this.props;
 
-    render() {
-        const { 
-            tickets, 
-            currency, 
-            stops: { allStops, noStops, oneStop, twoStops, threeStops }, 
-            mobileSidebar, 
-            asyncChangeSidebarView,
-        } = this.props;
-
-        return (
-            <div className="app">
-                <img className="app__logo" src={logo} alt="logo"/>
-                <div className="app__main">
-                    <SidebarContainer/>
-                    <div className="app__tickets">
-                        {tickets && !mobileSidebar ? 
-                        tickets
-                        .map(ticket => ({ ...ticket, price: ticket.price[currency] }))
-                        .filter(ticket => {
-                            return [allStops, noStops, oneStop, twoStops, threeStops]
-                            .map((state, index) => ({ state, index: index - 1 }))
-                            .filter(filter => filter.state)
-                            .map(filter => (filter.index === -1 || filter.index === ticket.stops))
-                            .filter(state => state)
-                            .length;
-                        })
-                        .map(ticket => <TicketContainer {...ticket} key={uniqueId()}/>)
-                        : null}
-                    </div>
-                </div>
-                {!mobileSidebar ?
-                <button className="app__button" onClick={asyncChangeSidebarView}>Фильтровать</button>
-                : null}
-            </div>
-        );
-    }
+    return (
+      <div className="app">
+        <img className="app__logo" src={logo} alt="logo" />
+        <div className="app__main">
+          <SidebarContainer />
+          <div className="app__tickets">
+            {tickets && !mobileSidebar
+              ? tickets
+                  .map(ticket => ({ ...ticket, price: ticket.price[currency] }))
+                  .filter(ticket => {
+                    return [allStops, noStops, oneStop, twoStops, threeStops]
+                      .map((state, index) => ({ state, index: index - 1 }))
+                      .filter(filter => filter.state)
+                      .map(
+                        filter =>
+                          filter.index === -1 || filter.index === ticket.stops
+                      )
+                      .filter(state => state).length;
+                  })
+                  .map(ticket => (
+                    <TicketContainer {...ticket} key={uniqueId()} />
+                  ))
+              : null}
+          </div>
+        </div>
+        {!mobileSidebar ? (
+          <button className="app__button" onClick={asyncChangeSidebarView}>
+            Фильтровать
+          </button>
+        ) : null}
+      </div>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-import AppContainer from './containers/App.js';
+import AppContainer from "./containers/App.js";
 
 const initState = {
-    tickets: [],
-    currency: 'RUB',
-    stops: {
-        allStops: true,
-        noStops: true,
-        oneStop: true,
-        twoStops: true,
-        threeStops: true,
-    },
-    mobileSidebar: false,
+  tickets: [],
+  currency: "RUB",
+  stops: {
+    allStops: true,
+    noStops: true,
+    oneStop: true,
+    twoStops: true,
+    threeStops: true
+  },
+  mobileSidebar: false
 };
 
 const store = createStore(
-    reducers,
-    initState,
-    composeWithDevTools(applyMiddleware(thunk)),
+  reducers,
+  initState,
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
-const render = document.getElementById('render');
+const render = document.getElementById("render");
 
 ReactDOM.render(
-    <Provider store={store}>
-      <AppContainer/>
-    </Provider>,
-    render,
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  render
 );
